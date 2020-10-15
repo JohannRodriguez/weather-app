399f6a1ce22b38fec3a015e6b134d36e687d181a
Lilith <themrblacksapce@hotmail.com>
2020-10-15 14:14:05 -0600

    Create methods to get user values

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('Hello World');
+const getUrl = (getCity, getState) => {
+  const urlData = {
+    root: 'http://api.openweathermap.org/data/2.5/weather?q=',
+    city: getCity,
+    state: getState,
+    key: '&APPID=a462784b129e8666735d11a68b50dc6c'
+  }
+  const {root, city, state, key} = urlData;
+  if (city.length === 0) {
+    return `${root}${state}${key}`;
+  } else if (state.length === 0) {
+    return `${root}${city}${key}`
+  } else {
+    return `${root}${city},${state}${key}`
+  }
+};
+
+const cap = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const convertNull = (string) => {
+  if(string === null){
+    return '';
+  } else {
+    return string;
+  }
+};
+
+const submitBtn = document.getElementById('submit-form');
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const getCity = cap(document.getElementById('city-name').value);
+  const getState = document.getElementById('state-name').value.toLowerCase();
+  const getPref = document.getElementById('temp-types').value;
+  const city = convertNull(getCity);
+  const state = convertNull(getState);
+  const url = getUrl(city, state);
+});
